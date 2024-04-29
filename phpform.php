35f3f489fa9d9a937841e1ac7c31986f1fc90b77
Albin RIVIERE <riviere.albin@gmail.com>
2024-04-29 11:25:44 +0200

    test

--- phpform.php
+++ phpform.php
@@ -1,54 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vitalab App</title>
+    <link rel="stylesheet" type="text/css" href="#"/>
+    <link rel="icon" href="#">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Connexion</h1>         
+</body>
+</html>
+
+
 <?php  
 
-echo "test";
+// echo "test";
 
 
-$serveur = "localhost";
-$dbname = "compta_frais";
-$user = "root";
-$pass = "";
+// $serveur = "localhost";
+// $dbname = "compta_frais";
+// $user = "root";
+// $pass = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupère les données du formulaire
-    $login = $_POST["id"];
-    $password = $_POST["password"];
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     // Récupère les données du formulaire
+//     $login = $_POST["id"];
+//     $password = $_POST["password"];
     
         
-    // Exemple d'affichage des données traitées
-    echo "id: " . $login. "<br>";
-    echo "mdp: " . $password;
-    }
+//     // Exemple d'affichage des données traitées
+//     echo "id: " . $login. "<br>";
+//     echo "mdp: " . $password;
+//     }
 
-    try{
+//     try{
 
-        $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
-        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
+//         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        //On vérifie si le login existe déjà et si le mot de passe correspond
-        $sth = $dbco->prepare("SELECT * FROM utilisateur WHERE Nom_utilisateur = :login AND MDP = :password");
-        $sth->bindParam('login',$login);
-        $sth->bindParam('password',$password);
-        $sth->execute();
-        $count = $sth->rowCount();
-        $role = $sth->fetchColumn(4);
-        if($count == 1 and $role == '1'){
-            echo 'Connexion réussie admin';
-           header("Location:admin.html");
-        }else if ($count == 1 and $role == '2'){
-            echo 'Connexion réussie comptable';
-            header("Location:comptable.html");
-        }else if ($count == 1 and $role == '3'){
-            echo 'Connexion réussie commercial';
-           header("Location:commercial.html");
-        }else{
-            echo 'Mauvais identifiants';
-            // Faire en sorte qu'on revienne sur la page HTML avec un message d'erreur   
-            header("Location:testh.html"); 
-        }
+//         //On vérifie si le login existe déjà et si le mot de passe correspond
+//         $sth = $dbco->prepare("SELECT * FROM utilisateur WHERE Nom_utilisateur = :login AND MDP = :password");
+//         $sth->bindParam('login',$login);
+//         $sth->bindParam('password',$password);
+//         $sth->execute();
+//         $count = $sth->rowCount();
+//         $role = $sth->fetchColumn(4);
+//         if($count == 1 and $role == '1'){
+//             echo 'Connexion réussie admin';
+//            header("Location:admin.html");
+//         }else if ($count == 1 and $role == '2'){
+//             echo 'Connexion réussie comptable';
+//             header("Location:comptable.html");
+//         }else if ($count == 1 and $role == '3'){
+//             echo 'Connexion réussie commercial';
+//            header("Location:commercial.html");
+//         }else{
+//             echo 'Mauvais identifiants';
+//             // Faire en sorte qu'on revienne sur la page HTML avec un message d'erreur   
+//             header("Location:testh.html"); 
+//         }
          
-        }
-        catch(PDOException $e){
-            echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
-        }
+//         }
+//         catch(PDOException $e){
+//             echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
+//         }
     
 ?>
