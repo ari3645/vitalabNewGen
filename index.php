bf5d2e3a6e49758cbd1c8cba1bf8156f84a224e5
Albin RIVIERE <riviere.albin@gmail.com>
2024-04-29 22:11:10 +0200

    test

--- index.php
+++ index.php
@@ -165,19 +165,23 @@
 ?>
 
 <?php
-// Informations de connexion à la base de données
-$serveur = "vitalab-new-gen.mysql.database.azure.com";
-$utilisateur = "albinrvi";
-$mot_de_passe = "Ari69.008";
-$base_de_donnees = "vitalab-new-gen";
+// Paramètres de connexion à la base de données
+$serveur = "adresse_du_serveur_mysql_azure";
+$utilisateur = "votre_nom_utilisateur";
+$mot_de_passe = "votre_mot_de_passe";
+$base_de_donnees = "nom_de_votre_base_de_donnees";
 
-echo "test";
-// Connexion à la base de données
-$connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
+// Tentative de connexion à la base de données
+$connexion = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
 
 // Vérifier la connexion
-if ($connexion->connect_error) {
-    die("La connexion a échoué : " . $connexion->connect_error);
+if (!$connexion) {
+    // Afficher un message d'erreur si la connexion échoue
+    echo "Erreur de connexion à la base de données : " . mysqli_connect_error();
+    // Arrêter l'exécution du script ou ajouter d'autres instructions pour gérer l'erreur
+    exit();
+} else {
+    // Connexion réussie, vous pouvez exécuter des requêtes SQL ou d'autres opérations ici
+    echo "Connexion réussie à la base de données !";
 }
-
-echo "Connexion réussie !";
+?>
