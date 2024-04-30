a6adebfc22e98cbc2d9dc4a02c4d55830cda93eb
Albin RIVIERE <riviere.albin@gmail.com>
2024-04-30 22:55:31 +0200

    final

--- index.php
+++ index.php
@@ -15,8 +15,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $login = $_POST["login"];
     $password = $_POST["password"];
 
-
-
     try {
         //On vérifie si le login existe déjà et si le mot de passe correspond
         $sth = $dbco->prepare("SELECT * FROM utilisateur WHERE Nom_utilisateur = :login AND mot_de_passe = :password");
