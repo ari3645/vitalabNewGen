5e099bbee2d412e53483dacc53b2ea2395a3b304
Albin RIVIERE <riviere.albin@gmail.com>
2024-04-30 10:35:12 +0200

    version

--- index.php
+++ index.php
@@ -1,45 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vitalab App</title>
-    <link rel="stylesheet" type="text/css" href="formulaire.css"/>
-    <link rel="icon" href="images/logo.png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
-    <div class="card">
-
-        <center><img src="images/logo.png" class="img-com-so" style="width: 18rem;"></center>
-        <p>&nbsp;</p>
-        <p>Identifiant</p>
-        <input type="text">
-       
-        <p>Mot de passe</p>
-        <input type="password">
-        
-        <center><a href="" class="bn1">Connexion</a></center>
-    </div>
-   
-        
-    
-</body>
-</html>
-
 <?php
-
-// echo "Hello World !";
-// Paramètres de connexion à la base de données
 $serveur = "vitalab-new-gen.mysql.database.azure.com";
-$utilisateur = "albinrvi";
-$mdp = "Ari69.008";
 $dbname = "vitalab-new-gen";
+$user = "albinrvi";
+$pass = "Ari69.008";
 
-$pdo = new PDO("mysql:host=$serveur;dbname=$dbname", $utilisateur, $mdp);
+$dbco = new PDO("mysql:host=$serveur;dbname=$dbname", $user, $pass);
 $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -47,11 +12,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $login = $_POST["login"];
     $password = $_POST["password"];
 
-
-// echo "Tentative de connexion.";
-// Tentative de connexion à la base de données
-
     try {
+        //On vérifie si le login existe déjà et si le mot de passe correspond
         $sth = $dbco->prepare("SELECT * FROM utilisateur WHERE Nom_utilisateur = :login AND mot_de_passe = :password");
         $sth->bindParam('login', $login);
         $sth->bindParam('password', $password);
@@ -75,11 +37,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location:testh.html");
             exit;
         }
-
     } catch (PDOException $e) {
-        echo "Erreur de connexion : " . $e->getMessage();
-        exit();
+        echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
     }
 }
-// Connexion réussie, vous pouvez exécuter des requêtes SQL ou d'autres opérations ici
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vitalab App</title>
+    <link rel="stylesheet" type="text/css" href="formulaire.css" />
+    <link rel="icon" href="#">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php echo '<div class="card">
+        <center><img src="images/logo.png" class="img-com-so" style="width: 18rem;"></center>
+        <form method="post" action=""> <!-- Ajout de la balise form -->
+            <p>&nbsp;</p>
+            <p>Identifiant</p>
+            <input type="text" name="login"> <!-- Ajout de l\'attribut name -->
+            <p>Mot de passe</p>
+            <input type="password" name="password"> <!-- Ajout de l\'attribut name -->
+            <center><button type="submit" class="bn1">Connexion</button></center> <!-- Remplacement du lien a par un bouton de type submit -->
+        </form>
+    </div>'; ?>
+
+</body>
+
+</html>
