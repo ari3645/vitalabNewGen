f5e3b54a7053d00dbcb7dca9adcb921775a2cfe0
Albin RIVIERE <riviere.albin@gmail.com>
2024-05-01 11:15:18 +0200

    final

--- commercial.php
+++ commercial.php
@@ -129,7 +129,7 @@ session_start();
             <p>Lieu : </p><input type="text" name="lieu">
             <p>Id Frais : </p><input type="number" name="id_frais">
             <p>Statut : </p><input type="text" name="statut">
-            <center><button class="bn1" type="submit">Ajouter</button></center>
+            <center><button class="bn1" type="submit">Modifier</button></center>
           </form>
         </div>
     </nav>
