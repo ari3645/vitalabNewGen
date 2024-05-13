a6080ad45756103756a9c9e0038d23d864855b7b
Albin RIVIERE <riviere.albin@gmail.com>
2024-05-13 10:26:45 +0200

    datatable

--- admin.php
+++ admin.php
@@ -14,7 +14,7 @@ session_start();
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
   </head>
 <body>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script>
         $(document).ready(function() {
@@ -26,7 +26,7 @@ session_start();
               paging: true //Activer la pagination
             });
         });
-    </script>
+    </script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
     <nav class="navbar">
@@ -205,7 +205,7 @@ session_start();
     <script src="chemin/vers/datatables.js"></script>
     <script>
         $(document).ready(function() {
-            $('#myTable').DataTable();
+            $('.display').DataTable();
         });
     </script>
 
